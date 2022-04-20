6c4e1ac8c78f131dfb4c77076c7d73545e6f2578
BrunoAquiCob <bruno.acerteaqui@gmail.com>
2022-04-20 12:54:09 -0300

    Separado as tabelas

---
+++ models/conversas.model.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, Sequelize) => {
+    const Conversas = sequelize.define("conversas", {
+      con_codigo: {
+        type: Sequelize.INTEGER,
+      },
+      con_fk_usu_codigo: {
+        type: Sequelize.INTEGER,
+      },
+      con_messagens: {
+        type: Sequelize.STRING,
+      },
+      con_data_hora: {
+        type: Sequelize.DATE,
+      },
+      con_cliente: {
+        type: Sequelize.STRING,
+      },
+    });
+    return Conversas;
+  };
+  
--- models/usuarios.model.js
+++ models/usuarios.model.js
@@ -40,22 +40,5 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING,
     },
   });
-  const Conversas = sequelize.define("conversas", {
-    con_codigo: {
-      type: Sequelize.INTEGER,
-    },
-    con_fk_usu_codigo: {
-      type: Sequelize.INTEGER,
-    },
-    con_messagens: {
-      type: Sequelize.STRING,
-    },
-    con_data_hora: {
-      type: Sequelize.DATE,
-    },
-    con_cliente: {
-      type: Sequelize.STRING,
-    },
-  });
-  return [Usuarios, Conversas];
+  return Usuarios;
 };

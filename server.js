d382244b04e872f9cfd05f81055ef7b766d9bfcc
BrunoAquiCob <bruno.acerteaqui@gmail.com>
2022-04-26 11:12:30 -0300

    Ajuste na ordem dos comandos do Express.

--- server.js
+++ server.js
@@ -3,25 +3,33 @@ const app = express();
 const port = 3005;
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const db = require("./models/db.model");
+const controler = require("./controllers/controller.js");
 let corsOptions = {
   origin: "http://localhost:3005",
 };
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
-});
-
-// set port, listen for requests
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
-});
 
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// set port, listen for requests
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+
+app.post("/createUsuario", (req, res) => {
+  controler.createUsuario(req, res);
+  res.json({ message: "Process finished!" });
+});
+
 db.sequelize.sync();
-db.sequelize.showAllSchemas();

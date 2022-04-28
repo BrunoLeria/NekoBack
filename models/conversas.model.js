3ae5b4f1d09f6c6658d0f78e6f915870b9f7bafe
BrunoAquiCob <bruno.acerteaqui@gmail.com>
2022-04-28 17:54:58 -0300

    Remove timestamps

--- models/conversas.model.js
+++ models/conversas.model.js
@@ -1,23 +1,33 @@
 module.exports = (sequelize, Sequelize) => {
-    const Conversas = sequelize.define("conversas", {
+  const Conversas = sequelize.define(
+    "conversas",
+    {
       con_codigo: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
-        primaryKey: true
+        primaryKey: true,
+        allowNull: false,
       },
       con_fk_usu_codigo: {
         type: Sequelize.INTEGER,
+        allowNull: false,
       },
       con_messagens: {
         type: Sequelize.STRING,
+        allowNull: false,
       },
       con_data_hora: {
         type: Sequelize.DATE,
+        allowNull: false,
       },
       con_cliente: {
         type: Sequelize.STRING,
+        allowNull: true,
       },
-    });
-    return Conversas;
-  };
-  
+    },
+    {
+      timestamps: false,
+    }
+  );
+  return Conversas;
+};
--- models/usuarios.model.js
+++ models/usuarios.model.js
@@ -1,46 +1,52 @@
 module.exports = (sequelize, Sequelize) => {
-  const Usuarios = sequelize.define("usuarios", {
-    usu_codigo: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      primaryKey: true
+  const Usuarios = sequelize.define(
+    "usuarios",
+    {
+      usu_codigo: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      usu_nome: {
+        type: Sequelize.STRING,
+      },
+      usu_email: {
+        type: Sequelize.STRING,
+      },
+      usu_senha: {
+        type: Sequelize.STRING,
+      },
+      usu_data_nascimento: {
+        type: Sequelize.DATEONLY,
+      },
+      usu_telefone: {
+        type: Sequelize.INTEGER,
+      },
+      usu_cep: {
+        type: Sequelize.INTEGER,
+      },
+      usu_endereco: {
+        type: Sequelize.STRING,
+      },
+      usu_numero: {
+        type: Sequelize.INTEGER,
+      },
+      usu_complemento: {
+        type: Sequelize.STRING,
+      },
+      usu_cidade: {
+        type: Sequelize.STRING,
+      },
+      usu_estado: {
+        type: Sequelize.STRING,
+      },
+      usu_foto: {
+        type: Sequelize.STRING,
+      },
     },
-    usu_nome: {
-      type: Sequelize.STRING,
-    },
-    usu_email: {
-      type: Sequelize.STRING,
-    },
-    usu_senha: {
-      type: Sequelize.STRING,
-    },
-    usu_data_nascimento: {
-      type: Sequelize.DATEONLY,
-    },
-    usu_telefone: {
-      type: Sequelize.INTEGER,
-    },
-    usu_cep: {
-      type: Sequelize.INTEGER,
-    },
-    usu_endereco: {
-      type: Sequelize.STRING,
-    },
-    usu_numero: {
-      type: Sequelize.INTEGER,
-    },
-    usu_complemento: {
-      type: Sequelize.STRING,
-    },
-    usu_cidade: {
-      type: Sequelize.STRING,
-    },
-    usu_estado: {
-      type: Sequelize.STRING,
-    },
-    usu_foto: {
-      type: Sequelize.STRING,
-    },
-  });
+    {
+      timestamps: false,
+    }
+  );
   return Usuarios;
 };

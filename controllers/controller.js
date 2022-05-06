742e33e5b59e9df2b86ef8196dc47f01196046ee
BrunoAquiCob <bruno.acerteaqui@gmail.com>
2022-05-06 15:08:11 -0300

    Ajuste de ordem no findAllTalk

--- controllers/controller.js
+++ controllers/controller.js
@@ -85,7 +85,7 @@ exports.findAllUser = async (req, res) => {
 // Retrieve all Talks from the database.
 exports.findAllTalk = (req, res) => {
   Talk.findAll({
-    order: [["tlk_chat_id", "tlk_data_hora"]],
+    order: [["tlk_chat_id"], ["tlk_date_time"]],
   })
     .then((data) => {
       if (data.length < 1 && data.every((talk) => talk instanceof Talk)) {
